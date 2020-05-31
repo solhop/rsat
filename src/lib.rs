41591701118369140adb20284705f39c1f7899a0
Prateek Kumar <prateek@prateekkumar.in>
2020-05-31 14:11:38 +0530

    Fix the doc tests
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,17 @@
 //! let options = SolverOptions::default();
 //! let mut solver = Solver::new(options);
 //! let vars: Vec<Var> = solver.new_vars(3);
-//! solver.add_clause(vec![vars[0].pos()]);
-//! solver.add_clause(vec![vars[1].neg()]);
-//! solver.add_clause(vec![vars[0].neg(), vars[1].pos(), vars[2].pos()]);
+//! solver.add_clause(vec![vars[0].pos_lit()]);
+//! solver.add_clause(vec![vars[1].neg_lit()]);
+//! solver.add_clause(vec![vars[0].neg_lit(), vars[1].pos_lit(), vars[2].pos_lit()]);
 //!
 //! assert_eq!(solver.solve(vec![]), Solution::Sat(vec![true, false, true]));
 //!
-//! assert_eq!(solver.solve(vec![vars[2].neg()]), Solution::Unsat);
+//! assert_eq!(solver.solve(vec![vars[2].neg_lit()]), Solution::Unsat);
 //!
 //! assert_eq!(solver.solve(vec![]), Solution::Sat(vec![true, false, true]));
 //!
-//! solver.add_clause(vec![vars[2].neg()]);
+//! solver.add_clause(vec![vars[2].neg_lit()]);
 //! assert_eq!(solver.solve(vec![]), Solution::Unsat);
 //! ```
 
